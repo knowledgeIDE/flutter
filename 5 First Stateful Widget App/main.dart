94de7d4eee3a7d815ea2141fdfc477e2281669e6
Shekhar <public.void.shekhar@gmail.com>
2019-02-19 22:06:19 +0530

    First Stateful Widget App

---
+++ 5 First Stateful Widget App/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+ 
+void main()
+{
+  runApp(new FirstFlutterApp());
+}
+
+class FirstFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App",
+      home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String strStringData = "Hello World";
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home Page"),
+      ),
+      body: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Text(strStringData),
+            new RaisedButton(
+              child: new Text("Click me"),
+                onPressed: onButtonPressed)
+          ],
+        ),
+      )
+    );
+  }
+
+  void onButtonPressed()
+  {
+    setState(() {
+      strStringData = "Welcome to Knowledge IDE";
+    });
+  }
+}
