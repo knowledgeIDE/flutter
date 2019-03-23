7200cb2b5ed9478157b821358e854900b8cf6874
Shekhar <public.void.shekhar@gmail.com>
2019-03-23 19:04:05 +0530

    8 Dark and Light Themes in Flutter

---
+++ 8 Dark and Light Themes/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+ 
+void main()
+{
+  runApp(new FirstFlutterApp());
+}
+
+class FirstFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App",
+      home: HomePage(),
+      theme: new ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.greenAccent,
+        accentColor: Colors.red
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String strStringData = "Hello World";
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Home Page"),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(strStringData,
+              style: new TextStyle(
+                  color: Colors.green,
+                  fontSize: 20.00
+              ),
+            ),
+            new RaisedButton(
+              child: new Text("Click me",
+                style: new TextStyle(
+                    color: Colors.white
+                ),),
+              onPressed: onButtonPressed,
+              color: Colors.red,
+            )
+          ],
+        ),
+      )
+    );
+  }
+
+  void onButtonPressed()
+  {
+    setState(() {
+      strStringData = "Welcome to Knowledge IDE";
+    });
+  }
+}
