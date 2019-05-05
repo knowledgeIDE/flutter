81132db60e85d53ea2cd7b0a841f0f96bf5fd84e
Shekhar <public.void.shekhar@gmail.com>
2019-05-05 14:44:02 +0530

    Custom Alert Dialog

---
+++ 11 Custom Alert Dialog/HomePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget
+{
+    @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Home Page",
+      home: new Text("Welcome in Knowledge IDE"),
+    );
+  }
+}
